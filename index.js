328e1fb2c99a0e8b2f43191762006a1dd1edcbca
Caio Louro <caio@tagme.com.br>
2017-07-06 14:04:48 -0300

    used http package instead of express

--- index.js
+++ index.js
@@ -1,10 +1,26 @@
-var express = require('express');
-var app = express();
+// var express = require('express');
+// var app = express();
 
-app.get('/', function (req, res) {
-    res.send('Hello World! I\'m Tagme!');
+// app.get('/', function (req, res) {
+//     res.send('Hello World! I\'m Tagme!');
+// });
+
+// app.listen(3000, function () {
+//     console.log('Example app listening on port 3000!');
+// });
+
+var http = require('http');
+
+var server = http.createServer(function (request, response) {
+
+    response.writeHead(200, {
+        "Content-Type": "text/plain"
+    });
+    response.end("Hello World!");
+
 });
 
-app.listen(3000, function () {
-    console.log('Example app listening on port 3000!');
-});
+var port = process.env.PORT || 1337;
+server.listen(port);
+
+console.log("Server running at http://localhost:%d", port);
